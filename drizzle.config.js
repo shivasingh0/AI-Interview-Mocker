b2d83a109b87b3b04cb331b65ea45fb7163778d1
shivasingh0 <shiva2003rajawat@gmail.com>
2024-07-21 23:49:11 +0530

    db Setup completed

---
+++ drizzle.config.js
@@ -0,0 +1,9 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.js",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: 'postgresql://AI-Interview-Mocker_owner:Shk1CFy5NMrJ@ep-odd-band-a58jx6ig.us-east-2.aws.neon.tech/AI-Interview-Mocker?sslmode=require',
+    }
+  };
+  
